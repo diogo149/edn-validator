b0f8a25c93646aad2364f2adc75b0ecfed4b09b9
Diogo Moitinho de Almeida <diogo149@gmail.com>
2014-05-17 09:22:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# edn-validator
+
+An edn validator that isolates errors to the smallest possible form. Useful for clojurescript errors.
+
+## Usage
+
+    $ lein run $FILENAME
---
+++ src/edn_validator/core.clj
@@ -0,0 +1,130 @@
+(ns edn-validator.core
+  (:require [clojure.tools.cli :as cli]
+            [instaparse.core :as insta]
+            [clojure.edn :as edn])
+  (:gen-class))
+
+(def cli-options
+  [["-h" "--help"]
+   ["-v" "--verbose" "Verbosity level."
+    :id :verbose?
+    :default false
+    :assoc-fn (fn [m k _] (assoc m k true))]])
+
+(defn usage
+  [options-summary]
+  (->> ["This is a validator for edn files."
+        ""
+        "Usage: program-name [options] edn-file"
+        ""
+        "Options:"
+        options-summary]
+       (clojure.string/join "\n")))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (clojure.string/join "\n" errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+;; Converting edn into it's subforms
+
+(def form-parser*
+  (insta/parser
+   "<S> = (form | neither)*
+    form = paren-form | bracket-form | brace-form
+    <paren-form> = '(' S ')'
+    <bracket-form> = '[' S ']'
+    <brace-form> = '{' S '}'
+    <neither> = #'[^\\[\\]\\(\\)\\{\\}]'"))
+
+(defn form-parser
+  [s]
+  (let [output (form-parser* s)]
+    (if (insta/failure? output)
+      (throw (Exception. (with-out-str (print output))))
+      output)))
+
+(defn form->nested
+  "Assumes that all form children are already in the output form (as in a
+   postwalk"
+  [p]
+  (cond
+   (keyword? p) nil ;; shouldn't matter, since the only keyword is :form
+   (string? p) {:string p :children nil :type :char}
+   (vector? p) (form->nested (rest p))
+   (seq? p) {:string (clojure.string/join (map :string p))
+             :children p
+             :type :form}))
+
+(defn edn->nested
+  [edn]
+  (->> edn
+       form-parser
+       (clojure.walk/postwalk form->nested)))
+
+;; Finding which forms have problems
+
+(defn edn-error
+  [s]
+  (try (edn/read-string s)
+       nil
+       (catch Exception e (.getMessage e))))
+
+(defn add-error
+  [f]
+  (if (and (map? f) (= :form (:type f)))
+    (assoc f :error (edn-error (:string f)))
+    f))
+
+(defn enrich-with-errors
+  [nested]
+  (clojure.walk/prewalk add-error nested))
+
+;; Isolating the problematic forms
+
+
+(defn isolate-errors
+  [nested]
+  (let [all-errors (atom []) ;; using at atom for a stateful walk
+        walk-fn (fn [x]
+                  (when (and (map? x) (:error x))
+                    (let [{:keys [string error children]} x
+                          children-errors (set (keep :error children))]
+                      (when-not (contains? children-errors error)
+                        (swap! all-errors conj {:string string :error error}))))
+                  x)]
+    (clojure.walk/prewalk walk-fn nested)
+    @all-errors))
+
+;; displaying
+
+(defn pprint-errors
+  [errors]
+  (doseq [{:keys [string error]} errors]
+    (println "Error occured in form: " string)
+    (println "With error message:    " error)
+    (println)))
+
+;; all together
+
+(defn validate-edn
+  [edn]
+  (->> edn
+       edn->nested
+       enrich-with-errors
+       isolate-errors
+       pprint-errors))
+
+(defn -main
+  [& args]
+  (let [{:keys [options arguments errors summary]}
+        (cli/parse-opts args cli-options)]
+    (cond
+     (:help options) (exit 0 (usage summary))
+     (not= (count arguments) 1) (exit 1 (usage summary))
+     errors (exit 1 (error-msg errors)))
+    ;; Execute program with options
+    (validate-edn (slurp (first arguments)))))
